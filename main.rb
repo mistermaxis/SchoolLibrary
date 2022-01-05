da5f8f1ba910b216dc9434342791f7d413c54732
Maximiliano <maximiliano_diaz@gmail.com>
2022-01-05 16:42:42 -0300

    Fix rubocop errors

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 require_relative 'book'
 require_relative 'rental'
 
-p = Person.new 25, 'Argyle', parent_permission: false
+p = Person.new 25, parent_permission: false, "Argyle"
 b = Book.new 'My grandma', 'Shakespeare'
 r = Rental.new '22-01-20', b, p
 
