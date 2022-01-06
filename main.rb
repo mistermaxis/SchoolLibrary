6c67daa5c83839eaf556ec7360b1e241369eaed8
Maximiliano <maximiliano_diaz@gmail.com>
2022-01-06 17:29:36 -0300

    First attempt creating UI

--- main.rb
+++ main.rb
@@ -1,10 +1,32 @@
 require_relative 'person'
 require_relative 'book'
-require_relative 'rental'
+require_relative 'display_list'
 
-p = Person.new 25, 'Argyle', parent_permission: false
-b = Book.new 'My grandma', 'Shakespeare'
-r = b.add_rental('20-12-2020', p)
+class App
+  include DisplayList
+  def initialize
+    @book_list = [Book.new('yes', 'no'), Book.new('maybe', 'yes')]
+    @people_list = []
+    @options = {
+      1 => self.display_books()
+    }
+  end
+  attr_reader :options
+end
 
-puts r.date
-p.rentals.each { |x| puts x.book.author }
+def main
+  school_library = App.new
+  puts "Welcome to School Library App!\n\n"
+  puts 'Please choose an option by enterin a number:'
+  print "1 - List all books
+2 - List all people
+3 - Create a person
+4 - Create a book
+5 - Create a rental
+6 - List all rentals for a given person id
+7 - Exit\n"
+  choice = gets.chomp
+  school_library.display_books()
+end
+
+main()
