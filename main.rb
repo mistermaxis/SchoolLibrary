fee5f4e541dc13434392fbf99ba90e8cb0956f87
Maximiliano <maximiliano_diaz@gmail.com>
2022-01-08 13:12:03 -0300

    Fix rubocop erros

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ require_relative 'ui'
 
 class App
   attr_reader :app_ui
+  
   def initialize
     @app_ui = UI.new
   end
--- ui.rb
+++ ui.rb
@@ -2,12 +2,15 @@ require_relative 'display_list'
 require_relative 'create_item'
 
 class UI
-  include DisplayList, CreateItem
+  include DisplayList
+  include CreateItem
+
   def initialize
-    @book_list = [Book.new('yes', 'no'), Book.new('maybe', 'yes')]
-    @people_list = [Person.new(11, 0, 'yes', parent_permission: false), Person.new(44, 1, 'maybe', parent_permission: true)]
+    @book_list = []
+    @people_list = []
     @input = ''
   end
+
   def display_ui
     while @input != '7'
         puts "Welcome to School Library App!\n\n"
@@ -20,24 +23,28 @@ class UI
         puts '6 - List all rentals from a given person id'
         puts '7 - Exit'
         @input = gets.chomp
-        case @input
-        when '1'
-          display_books
-        when '2'
-          display_people
-        when '3'
-          create_person
-        when '4'
-          create_book
-        when '5'
-          create_rental
-        when '6'
-          display_rentals
-        when '7'
-          break
-        else
-          print "Invalid choice\n"
-        end
+        handle_input
+    end
+  end
+
+  def handle_input
+    case @input
+    when '1'
+      display_books
+    when '2'
+      display_people
+    when '3'
+      create_person
+    when '4'
+      create_book
+    when '5'
+      create_rental
+    when '6'
+      display_rentals
+    when '7'
+      
+    else
+      print "Invalid choice\n"
     end
   end
 end
