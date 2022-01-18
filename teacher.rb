8f41a2366c3ffe719d4349c86dbfc2e9a2c275d0
Maximiliano <maximiliano_diaz@gmail.com>
2022-01-18 17:34:56 -0300

    Add to_json method to Teacher class

--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,21 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(name, id, parent_permission, age, specialization)
-    super(age, id, name, parent_permission: parent_permission)
+  def initialize(name, id, age, specialization)
+    super(age, id, name)
     @specialization = specialization
   end
 
+  attr_accessor :specialization
+
   def can_use_services?
     true
+  end
+
+  def to_json(*args)
+  {
+    JSON.create_id  => self.class.name,
+    'a'             => [ name, id, age, specialization ]
+  }.to_json(*args)
   end
 end
