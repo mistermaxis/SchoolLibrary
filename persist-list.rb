fd0e6c9ce6e52cc357c68dd76a4ec550d4d77946
Maximiliano <maximiliano_diaz@gmail.com>
2022-01-18 17:35:31 -0300

    Add code for persist methods

--- persist-list.rb
+++ persist-list.rb
@@ -1,11 +1,13 @@
+require 'json'
+
 module PersistList
   def persist_books
-    
+    File.write("books.json", JSON.dump(@book_list))
   end
   def persist_people
-    
+    File.write("people.json", JSON.dump(@people_list))
   end
   def persist_rentals
-    
+    File.write("rentals.json", JSON.dump(@rental_list))
   end
 end
