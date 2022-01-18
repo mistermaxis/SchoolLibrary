aa071326d2b0553a27ddc3e12befa00aeb41dadf
Maximiliano <maximiliano_diaz@gmail.com>
2022-01-18 17:25:42 -0300

    Add to_json method to Student class

--- student.rb
+++ student.rb
@@ -2,10 +2,11 @@ require_relative 'person'
 require_relative 'classroom'
 
 class Student < Person
-  attr_reader :classroom
+  attr_reader :classroom, :parent_permission
 
   def initialize(name, id, age, parent_permission)
-    super(age, id, name, parent_permission: parent_permission)
+    super(age, id, name)
+    @parent_permission = parent_permission
     new_classroom = Classroom.new('None')
     @classroom = new_classroom
     new_classroom.students.push(self) unless new_classroom.students.include?(self)
@@ -18,5 +19,12 @@ class Student < Person
 
   def play_hooky
     '¯\\(ツ)/¯'
+  end
+
+  def to_json(*args)
+  {
+    JSON.create_id  => self.class.name,
+    'a'             => [ name, id, age, parent_permission ]
+  }.to_json(*args)
   end
 end
