8df45fc22e08043b18b4f06d6986584e8c5c5494
Maximiliano <maximiliano_diaz@gmail.com>
2022-01-19 13:10:37 -0300

    Create unit test for Student class

---
+++ spec/student_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../src/student'
+
+describe Student do
+  context 'When testing the Student class' do
+    it "should return a new Student instance with the supplied name, age and permission" do
+      student = Student.new('Billy', 777, 23, false)
+      name = student.name
+      id = student.id
+      age = student.age
+      parent_permission = student.parent_permission
+      expect(name).to eq 'Billy'
+      expect(id).to eq 777
+      expect(age).to eq 23
+      expect(parent_permission).to eq false
+    end
+  end
+end
