fc78d880cda8cdbe3c5f0336147fadd30af69014
Maximiliano <maximiliano_diaz@gmail.com>
2022-01-18 21:05:02 -0300

    Fix rubocop errors

--- src/load_list.rb
+++ src/load_list.rb
@@ -3,6 +3,7 @@ require 'json'
 module LoadList
   def load_books
     return unless File.exist?('JSON/books.json')
+
     books = File.open('JSON/books.json').read
     books_data = JSON.parse(books, create_additions: true)
     @book_list = books_data
@@ -10,6 +11,7 @@ module LoadList
 
   def load_people
     return unless File.exist?('JSON/people.json')
+
     people = File.open('JSON/people.json').read
     people_data = JSON.parse(people, create_additions: true)
     people_data.each { |prs| @people_list.push(prs) }
@@ -17,6 +19,7 @@ module LoadList
 
   def load_rentals
     return unless File.exist?('JSON/rentals.json')
+    
     rentals = File.open('JSON/rentals.json').read
     rentals_data = JSON.parse(rentals, create_additions: true)
     rentals_data.each { |rnt| @rental_list.push(rnt) }
